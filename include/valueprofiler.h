47e2f13d877f12d2b7ec637926ff2d2ac709108f
Siddharth Bhat <siddu.druid@gmail.com>
2017-11-23 21:02:44 +0100

    change dump_values to have vp_prefix

--- include/valueprofiler.h
+++ include/valueprofiler.h
@@ -2,5 +2,5 @@
 extern "C" {
     // This is a convenince API for polly.
     void vp_profile_value(const char *uniquename, uint64_t value);
-    void dump_values(const char*filename);
+    void vp_dump_values(const char*filename);
 }
